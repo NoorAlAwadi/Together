8a9fcf835d1075c2359e32ff823501ab13194ef6
MobileProg <mobileprog@bp-36a-103-04.local>
2019-04-04 16:52:41 +0300

    Initial Commit

---
+++ Together/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Together
+//
+//  Created by MobileProg on 4/4/19.
+//  Copyright © 2019 MobileProg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
