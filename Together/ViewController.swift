95ffee30c1c3d2fe66df5de252d373b9a3f18ada
Mobile Prog-Class1 <mobileprog-class1@bp-36-201-02.local>
2019-05-27 13:12:52 +0300

    initial commit

--- Together/ViewController.swift
+++ Together/ViewController.swift
@@ -12,9 +12,45 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+//        // Create a white border with defined width
+//        imgView.layer.borderColor = [UIColor Black].CGColor
+//        imgView.layer.borderWidth = 1.5;
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
+    
+   
+    @IBOutlet var cityButtons: [UIButton]!
+    
+    @IBAction func handleSelection(_ sender: UIButton) {
+    
+        cityButtons.forEach { (button) in
+            UIView.animate(withDuration: 0.3, animations: {
+                
+                 button.isHidden = !button.isHidden
+                })
+           
+        }
+        
+       
+       
+    }
+    
+    @IBAction func cityTapped(_ sender: UIButton) {
+    }
+    
+    
+//    let layer = CAGradientLayer()
+//    layer.frame = view.bounds
+//    layer.colors = [UIColor.yellow.cgColor, UIColor.green.cgColor]
+//    layer.startPoint = CGPoint(0,0)
+//    layer.stopPoint = CGPoint(1,1)
+//    view.layer.addSubLayer(layer)
 
+
+    
 
 }
 
